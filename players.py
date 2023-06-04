eb415381d98c097f64306c127d637fdd9ac05fe5
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-06-04 05:41:04 -0600

    Created Dealer and User classes

--- players.py
+++ players.py
@@ -1,6 +1,7 @@
 import cards
 
 class Player:
+    '''Purpose:create a base class for dealer and the_player to inherit'''
     def __init__(self):
         self.value = 0
         self.cards = []
@@ -17,4 +18,20 @@ class Player:
         '''Purpose:to check to see if Player has bust
         Post-conditions: will change the value of self.is_bust to True if over 21
         '''
-        if value > 21 and :
+        if value > 21 and "A" in self.cards:
+            value -=10
+        elif value > 21:
+            self.is_bust = True
+    
+
+class Dealer(Player):
+    pass
+
+class User(Player):
+    pass
+
+
+
+
+    
+
