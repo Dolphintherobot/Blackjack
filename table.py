c2019d13a1e1efed9df845868d2a1be7c46e604b
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-06-04 06:52:46 -0600

    Created start game function, added key functionality in main function.

--- table.py
+++ table.py
@@ -1,6 +1,6 @@
-import cards
+import cards as c
 import pygame
-import players
+import players as p
 
 def hit(a_player,deck):
     '''Purpose:to deal another card to an instance of Players class
@@ -10,6 +10,31 @@ def hit(a_player,deck):
     card = deck.deal()
     a_player.add_card(card)
     a_player.over_21()
+
+
+def start_game(player:p.Player,dealer:p.Player,deck:c.Deck,image_collection:c.Images,screen):
+    '''Purpose: to start the game
+    :param player: instance of player class, representing the player
+    :param dealer: instance of player class, representing the dealer
+    :param deck: instance of deck class, representing card deck
+    :param image_collection: instance of image class
+    :param screen: surface to be drawn onto'''
+    deck.load_deck()
+
+    player_coord = (600,800)
+    dealer_coord = (600,400)
+    n = 0
+    while n != 2:
+        n+=1
+        dealer_card = deck.deal()
+        player_card = deck.deal()
+
+        player_card.move_image(screen,player_coord,image_collection)
+        dealer_card.move_image(screen,dealer_coord,image_collection)
+        player_coord = (player_coord[0] -100,800)
+        dealer_coord = (dealer_coord[0]-100,400)
+        player.add_card(player_card)
+        dealer.add_card(dealer_card)
 
 
 
@@ -22,18 +47,21 @@ def main():
     pygame.init()
     size = (1000, 1000)
     screen = pygame.display.set_mode(size)
-    
     done = False
     clock = pygame.time.Clock()
-
-
-    the_card = C.Card("H7")
     GREEN = (0,255,0)
-    # background = pygame.Surface(screen.get_size())
+
+
+    dealer = p.Player()
+    user = p.Player()
+    card_deck = c.Deck()
+    image_collection = c.Images()
+    
+    
     
 
     
-    
+    # start_game(user,dealer,card_deck,image_collection,screen)
 
     
     while not done:
@@ -42,12 +70,21 @@ def main():
                 done = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    the_card.move_image(screen,(1000,1))
-
+                    #hit
+                    pass
+                if event.key == pygame.K_LSHIFT or pygame.K_RSHIFT:
+                    #stay
+                    pass
+                if event.key == pygame.K_LCTRL or pygame.K_RCTRL:
+                    start_game(user,dealer,card_deck,image_collection,screen)
+                    
+                if event.key == pygame.K_ESCAPE:
+                    done = True
         
-        # screen.blit(background,(0,0))
+        
         screen.fill(GREEN)
-        the_card.draw_image(screen)
+        image_collection.update(screen)
+        
     
         
         
@@ -59,3 +96,7 @@ def main():
  
 
     pygame.quit()
+
+
+
+main()
