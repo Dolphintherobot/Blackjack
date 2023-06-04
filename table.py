76f83635e7f60bd002e43816e942548878987aa2
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-06-04 05:59:54 -0600

    Created main function.

--- table.py
+++ table.py
@@ -11,3 +11,51 @@ def hit(a_player,deck):
     a_player.add_card(card)
     a_player.over_21()
 
+
+
+
+
+def main():
+    '''
+    Purpose:program that runs the gui
+    '''
+    pygame.init()
+    size = (1000, 1000)
+    screen = pygame.display.set_mode(size)
+    
+    done = False
+    clock = pygame.time.Clock()
+
+
+    the_card = C.Card("H7")
+    GREEN = (0,255,0)
+    # background = pygame.Surface(screen.get_size())
+    
+
+    
+    
+
+    
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    the_card.move_image(screen,(1000,1))
+
+        
+        # screen.blit(background,(0,0))
+        screen.fill(GREEN)
+        the_card.draw_image(screen)
+    
+        
+        
+
+        
+        
+        pygame.display.flip()
+        clock.tick(60)
+ 
+
+    pygame.quit()
