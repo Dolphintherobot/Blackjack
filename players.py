2a72b106404a1d911a5d7beade474e7f5fb41eb8
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-06-04 05:48:32 -0600

    Changed all variables named value to self.value to fix UnboundLocalError.

--- players.py
+++ players.py
@@ -11,16 +11,17 @@ class Player:
         '''Purpose:to add a card into Players hand
         :param card: a card object'''
 
-        value += card.value
+
+        self.value += card.value
         self.cards.append(card.card[1:])
     
     def over_21(self):
         '''Purpose:to check to see if Player has bust
         Post-conditions: will change the value of self.is_bust to True if over 21
         '''
-        if value > 21 and "A" in self.cards:
-            value -=10
-        elif value > 21:
+        if self.value > 21 and "A" in self.cards:
+            self.value -=10
+        elif self.value > 21:
             self.is_bust = True
     
 
